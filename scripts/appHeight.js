da9bb9f108b3ead714c88d61dd51a7e486354f6f
Ali Jamali <ali@ali-m1.local>
2021-05-24 22:48:10 -0700

    test mobile bt11

--- scripts/appHeight.js
+++ scripts/appHeight.js
@@ -4,11 +4,3 @@ const appHeight = () => {
 }
 window.addEventListener('resize', appHeight)
 appHeight()
-
-
-// const appWidth = () => {
-//     const doc = document.documentElement
-//     doc.style.setProperty('--app-width', `${window.innerWidth}px`)
-// }
-// window.addEventListener('resize', appWidth)
-// appWidth()
